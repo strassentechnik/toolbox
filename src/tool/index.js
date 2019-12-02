900f55a984be16200b1fee2d0af7b07856c734e8
philippmeissner <pm@zeit.io>
2019-12-02 11:04:37 +0100

    removed unused properties from form, use props instead

--- src/tool/index.js
+++ src/tool/index.js
@@ -1,42 +1,42 @@
 function getCurrentScript() {
-  const scripts = document.getElementsByTagName('script')
-  return scripts[scripts.length - 1]
+  const scripts = document.getElementsByTagName('script');
+  return scripts[scripts.length - 1];
 }
 
-;(function createIFrame() {
-  const currentScript = document.currentScript || getCurrentScript()
-  const mode = currentScript.getAttribute('mode') || 'production'
-  const debug = currentScript.getAttribute('debug') || false
-
-  const iframe = document.createElement('iframe')
+(function createIFrame() {
+  const currentScript = document.currentScript || getCurrentScript();
+  const productId = currentScript.getAttribute('product_id') || null;
+  const target = currentScript.getAttribute('target') || null;
+  const mode = currentScript.getAttribute('mode') || 'production';
+  const debug = currentScript.getAttribute('debug') || false;
+  const iframe = document.createElement('iframe');
 
   const iframeSrc =
     mode === 'production' || mode === 'staging'
       ? 'https://strassentechnik.github.io/toolbox/toolbox.html'
-      : 'http://localhost:8080/toolbox/toolbox.html'
+      : 'http://localhost:8080/toolbox/toolbox.html';
 
-  iframe.scrolling = 'no'
-  iframe.setAttribute('allowTransparency', true)
-  iframe.frameBorder = '0'
-  iframe.src = `${iframeSrc}?theme=amtec&mode=${mode}`
+  iframe.scrolling = 'yes';
+  iframe.setAttribute('allowTransparency', true);
+  iframe.frameBorder = '0';
+  const productIdParam = !!productId ? `&product_id=${productId}` : '';
+  const iframeParams = `?theme=amtec&mode=${mode}&target=${target}${debug ? '&debug=true' : ''}${productIdParam}`;
+  iframe.src = `${iframeSrc}${iframeParams}`;
 
   if (debug) {
-    console.log('[nadler-tools]', mode, iframeSrc)
-    iframe.src = `${iframeSrc}?theme=amtec&mode=${mode}&debug=true`
-  } else {
-    iframe.src = `${iframeSrc}?theme=amtec&mode=${mode}`
+    console.log('[nadler-tools]', mode, iframeSrc, iframeParams);
   }
 
   window.addEventListener(
     'message',
-    ({ data }) => {
+    ({data}) => {
       if (data.height) {
-        iframe.style.width = '100%'
-        iframe.style.height = data.height + 'px'
+        iframe.style.width = '100%';
+        iframe.style.height = data.height + 'px';
       }
     },
-    false
-  )
+    false,
+  );
 
-  currentScript.parentNode.insertBefore(iframe, currentScript.nextSibling)
-})()
+  currentScript.parentNode.insertBefore(iframe, currentScript.nextSibling);
+})();
--- src/toolbox/Configurator/Configurator.jsx
+++ src/toolbox/Configurator/Configurator.jsx
@@ -1,46 +1,58 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import axios from 'axios'
-import ConfiguratorForm from './components/ConfiguratorForm'
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import ConfiguratorForm from './components/ConfiguratorForm';
 
 const targetUrls = {
   development: 'http://localhost:3000/messages.json',
   staging: 'https://nadler-staging.herokuapp.com/messages.json',
   production: 'https://www.strassentechnik.de/messages.json',
-}
+};
 
 export default class Configurator extends React.Component {
   static propTypes = {
     mode: PropTypes.oneOf(['development', 'staging', 'production']),
-  }
+    target: PropTypes.string,
+    product_id: PropTypes.string,
+  };
 
-  getTargetUrl = () => targetUrls[this.props.mode]
+  getTargetUrl = () => targetUrls[this.props.mode];
+
+  buildParams = (formValues) => {
+    return {
+      message: {
+        ...formValues,
+        product_id: this.props.product_id,
+      },
+      target: this.props.target,
+    };
+  };
 
   handleSubmit = (values, form, complete) =>
     axios
-      .post(this.getTargetUrl(), { message: values })
+      .post(this.getTargetUrl(), this.buildParams(values))
       .then(resp => {
-        form.reset()
-        complete()
+        form.reset();
+        complete();
         if (resp.data && resp.data.target) {
           setTimeout(() => {
             // Send message to outer window so that we can track it accordingly
             window.parent.postMessage(
-              { event_id: 'configComplete', target: resp.data.target },
-              '*'
-            )
-          }, 3500)
+              {event_id: 'configComplete', target: resp.data.target},
+              '*',
+            );
+          }, 3500);
         }
       })
       .catch(error => {
-        complete(error)
-      })
+        complete(error);
+      });
 
   render() {
     return (
       <div>
-        <ConfiguratorForm onSubmit={this.handleSubmit} />
+        <ConfiguratorForm onSubmit={this.handleSubmit}/>
       </div>
-    )
+    );
   }
 }
